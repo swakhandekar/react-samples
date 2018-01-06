8e56774b5902c1327fcf5687d062f74a3a792707
Swapnil Khandekar <swa.khandekar@gmail.com>
2018-01-06 17:25:48 +0530

    Add error message in InputField

--- __tests__/components/form/after/InputField.test.jsx
+++ __tests__/components/form/after/InputField.test.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import {shallow} from "enzyme";
 
 describe('InputField', () => {
-    let type, placeholder, label;
+    let type, placeholder, label, errorMessage, invalidField;
     let component;
 
     const getComponent = () => shallow(
@@ -11,29 +11,47 @@ describe('InputField', () => {
             label={label}
             type={type}
             placeholder={placeholder}
+            errorMessage={errorMessage}
+            invalidField={invalidField}
         />
     );
 
     beforeEach(() => {
         type = 'text';
-        placeholder = 'default placeholder';
-        label = 'default label';
     });
 
     it('should have a input field with passed type prop as type', () => {
+        placeholder = 'default placeholder';
         component = getComponent();
-
         const input = component.find('input');
+
         expect(input).toHaveLength(1);
         expect(input.props().type).toBe(type);
         expect(input.props().placeholder).toBe(placeholder);
     });
 
     it('should have a label if passed', () => {
+        label = 'default label';
         component = getComponent();
 
-        let label = component.find('label');
-        expect(label).toHaveLength(1);
-        expect(label.text()).toBe('default label: ');
+        const labelField = component.find('label');
+        expect(labelField).toHaveLength(1);
+        expect(labelField.text()).toBe('default label: ');
+    });
+
+    it('should show passed error message if the invalidField flag is true', () => {
+        invalidField = true;
+        errorMessage = "Some error";
+        component = getComponent();
+
+        expect(component.find('.error-message').text()).toBe(errorMessage)
+    });
+
+    it('should not show error message if the invalidField flag is false', () => {
+        invalidField = false;
+        errorMessage = 'Some error';
+        component = getComponent();
+
+        expect(component.find('.error-message')).toHaveLength(0);
     });
 });
--- src/components/form/after/InputField.jsx
+++ src/components/form/after/InputField.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const InputField = ({label, type, placeholder}) => (
+export const InputField = ({label, type, placeholder, errorMessage, invalidField}) => (
     <div>
         <label>{label}: </label>
         <input type={type} placeholder={placeholder}/>
+        {invalidField &&
+            <div className='error-message'>
+                <span>{errorMessage}</span>
+            </div>
+        }
     </div>
 );
 
@@ -12,4 +17,11 @@ InputField.propTypes = {
     label: PropTypes.string,
     type: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
+    invalidField: PropTypes.bool,
+    errorMessage: PropTypes.string,
+};
+
+InputField.defaultProps = {
+    invalidField: false,
+    errorMessage: "",
 };
